a7c84bbad7f25f251cc9e2c07e11d63f6d4fa7b0
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-23 14:02:01 -0500

    overridden service interface methods

--- src/main/java/com/revature/project2/services/OrderService.java
+++ src/main/java/com/revature/project2/services/OrderService.java
@@ -1,9 +1,38 @@
 package com.revature.project2.services;
 
-public class OrderService implements Service {
+import com.revature.project2.models.Order;
+
+import java.util.Set;
+
+public class OrderService implements Service<Order> {
 
     public OrderService() {
         System.out.println("OrderService constructor");
     }
 
+
+    @Override
+    public Order save(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Set<Order> findAll() {
+        return null;
+    }
+
+    @Override
+    public Order update(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order delete(Order order) {
+        return null;
+    }
 }
--- src/main/java/com/revature/project2/services/Service.java
+++ src/main/java/com/revature/project2/services/Service.java
@@ -1,8 +1,20 @@
 package com.revature.project2.services;
 
-public interface Service {
+import java.util.Set;
+
+public interface Service<T> {
 
     // CRUD operations
     // save, find, update, delete
+
+    T save (T t);
+
+    T findById (Integer id);
+
+    Set<T> findAll ();
+
+    T update (T t);
+
+    T delete (T t);
 
 }
