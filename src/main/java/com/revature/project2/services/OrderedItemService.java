56ba986cb31f355c745991a1603c7b5baa7e26c4
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-23 08:04:36 -0500

    OrderedItemsService created

---
+++ src/main/java/com/revature/project2/services/OrderedItemService.java
@@ -0,0 +1,9 @@
+package com.revature.project2.services;
+
+public class OrderedItemService implements Service {
+
+    public OrderedItemService() {
+        System.out.println("OrderedItemService constructor");
+    }
+
+}
