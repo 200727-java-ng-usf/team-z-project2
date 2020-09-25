55cee9a8726de565dc5299bc8289f755c9c0c949
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-25 09:24:21 -0500

    OrderedItemController CREATE and READ methods ready to comment in

--- src/main/java/com/revature/project2/web/controllers/OrderedItemController.java
+++ src/main/java/com/revature/project2/web/controllers/OrderedItemController.java
@@ -1,12 +1,12 @@
 package com.revature.project2.web.controllers;
 
 import com.revature.project2.models.OrderedItem;
+import com.revature.project2.models.User;
 import com.revature.project2.services.OrderedItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,7 +19,20 @@ public class OrderedItemController {
     @Autowired
     public OrderedItemController (OrderedItemService orderedItemService) { this.orderedItemService = orderedItemService; }
 
+    // CREATE operation
+//    @ResponseStatus(HttpStatus.CREATED)
+//    @PostMapping(produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+//    public OrderedItem registerNewOrderedItem (@RequestBody OrderedItem newOrderedItem) {
+//        return orderedItemService.save(newOrderedItem);
+//    }
+
+    // READ operation
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<OrderedItem> getAllOrderedItems() { return orderedItemService.findAll(); }
+
+    // READ operation
+    // specify value param so that /users get request is not ambiguous
+//    @GetMapping(value="id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public User getOrderedItemById (@PathVariable int id) { return orderedItemService.findById(id); }
 
 }
