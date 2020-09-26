9e6fdb246965c5536f37b6a3d02b57050af186b8
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-25 22:29:33 -0500

    Made authenticate method with dtos that uses UserRepo findUserByUsernameAndPassword method.

--- src/main/java/com/revature/project2/services/UserService.java
+++ src/main/java/com/revature/project2/services/UserService.java
@@ -1,9 +1,13 @@
 package com.revature.project2.services;
 
+import com.revature.project2.exceptions.AuthenticationException;
+import com.revature.project2.exceptions.InvalidRequestException;
 import com.revature.project2.exceptions.ResourceNotFoundException;
 import com.revature.project2.models.Role;
 import com.revature.project2.models.User;
 import com.revature.project2.repos.UserRepository;
+import com.revature.project2.web.dtos.Credentials;
+import com.revature.project2.web.dtos.Principal;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
@@ -59,6 +63,27 @@ public class UserService {
     public User findUserByUsernameAndPassword(String username, String password) {
 
         return userRepo.findUserByUsernameAndPassword(username,password).orElseThrow(ResourceNotFoundException::new);
+
+    }
+
+    @Transactional
+    public Principal authenticate (Credentials creds) {
+
+        if (creds == null || creds.getUsername() == null || creds.getPassword() == null
+                || creds.getUsername().equals("") || creds.getPassword().equals("")) {
+            throw new InvalidRequestException("Invalid credentials object provided!");
+        }
+
+        try {
+
+            User authUser = userRepo.findUserByUsernameAndPassword(creds.getUsername(), creds.getPassword())
+                    .orElseThrow(AuthenticationException::new);
+
+            return new Principal(authUser);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
