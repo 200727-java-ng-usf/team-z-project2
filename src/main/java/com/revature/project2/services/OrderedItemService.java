03c72b09902772f36fb0e0766a5bfc1b4e96987f
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-26 15:06:04 -0500

    Gu's changes to OrderedItemService

--- src/main/java/com/revature/project2/services/OrderedItemService.java
+++ src/main/java/com/revature/project2/services/OrderedItemService.java
@@ -1,7 +1,10 @@
 package com.revature.project2.services;
 
+import com.revature.project2.exceptions.ResourceNotFoundException;
+import com.revature.project2.models.Item;
 import com.revature.project2.models.Order;
 import com.revature.project2.models.OrderedItem;
+import com.revature.project2.models.User;
 import com.revature.project2.repos.OrderRepository;
 import com.revature.project2.repos.OrderedItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -26,4 +30,34 @@ public class OrderedItemService {
     public List<OrderedItem> findAll() {
         return orderedItemRepo.findAll();
     }
+
+
+    @Transactional
+    public OrderedItem save(OrderedItem newOrderedItem) {
+        return orderedItemRepo.save(newOrderedItem).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @Transactional
+    public OrderedItem findById(Integer id) {
+        return orderedItemRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
+    }
+
+    @Transactional
+    public List<OrderedItem> findOrderedItemsByItem(Item item) {
+        return orderedItemRepo.findOrderedItemsByItem(item);
+    }
+
+
+    @Transactional
+    public boolean update(OrderedItem updateObj) {
+        return orderedItemRepo.update(updateObj);
+    }
+
+    @Transactional
+    public boolean deleteById(Integer id) {
+        return orderedItemRepo.deleteById(id);
+    }
+
+
+
 }
