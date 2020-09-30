4ece84e05d83cb511fdcf523140711b2371b944c
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-09-30 17:14:44 -0500

    update import.sql 2

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,19 +1,19 @@
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (1, 'John', 'password', 'John', 'Smith', 'josmith@gmail.com', 'USER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (2, 'Jacob', 'password', 'Jacob', 'Smith', 'jasmith@gmail.com', 'USER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (3, 'Jingle', 'password', 'Jingle', 'Smith', 'jismith@gmail.com', 'USER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (4, 'Dasher', 'password', 'Dasher', 'Smith', 'dasher@gmail.com', 'USER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (5, 'Dancer', 'password', 'Dancer', 'Smith', 'dancer@gmail.com', 'USER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (6, 'Prancer', 'password', 'Prancer', 'Smith', 'prancer@gmail.com', 'MANAGER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (7, 'Vincent', 'password', 'Vincent', 'Smith', 'vincent@gmail.com', 'MANAGER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (8, 'Comet', 'password', 'Comet', 'Smith', 'comet@gmail.com', 'MANAGER');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (9, 'Cupid', 'password', 'Cupid', 'Smith', 'cupid@gmail.com', 'ADMIN');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (10, 'Donner', 'password', 'Donner', 'Smith', 'donner@gmail.com', 'ADMIN');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (11, 'Blitzen', 'password', 'Blitzen', 'Smith', 'blitzen@gmail.com', 'ADMIN');
-insert into mall_user (id, username, password, firstName, lastName, email, role) values (12, 'Rudolph', 'password', 'Rudolph', 'Smith', 'rudolph@gmail.com', 'INACTIVE');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'John', 'password', 'John', 'Smith', 'josmith@gmail.com', 'USER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Jacob', 'password', 'Jacob', 'Smith', 'jasmith@gmail.com', 'USER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Jingle', 'password', 'Jingle', 'Smith', 'jismith@gmail.com', 'USER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Dasher', 'password', 'Dasher', 'Smith', 'dasher@gmail.com', 'USER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Dancer', 'password', 'Dancer', 'Smith', 'dancer@gmail.com', 'USER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Prancer', 'password', 'Prancer', 'Smith', 'prancer@gmail.com', 'MANAGER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Vincent', 'password', 'Vincent', 'Smith', 'vincent@gmail.com', 'MANAGER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Comet', 'password', 'Comet', 'Smith', 'comet@gmail.com', 'MANAGER');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Cupid', 'password', 'Cupid', 'Smith', 'cupid@gmail.com', 'ADMIN');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Donner', 'password', 'Donner', 'Smith', 'donner@gmail.com', 'ADMIN');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Blitzen', 'password', 'Blitzen', 'Smith', 'blitzen@gmail.com', 'ADMIN');
+insert into mall_user (username, password, firstName, lastName, email, role) values ( 'Rudolph', 'password', 'Rudolph', 'Smith', 'rudolph@gmail.com', 'INACTIVE');
 
-insert into item_hats (name, price, stock, description, itemImageUrl, genre) values (1, "Sunhat 3000", 29.99, 20, "Keep the sun out of your face!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_6.jpg", "SUN");
-insert into item_hats (name, price, stock, description, itemImageUrl, genre) values (2, "Baseball M Hat", 59.99, 10, "Super cool!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_1.jpg", "BASEBALL");
-insert into item_hats (name, price, stock, description, itemImageUrl, genre) values (3, "Rodeo Hat", 23.99, "Square dancin'!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_2.jpg", "COWBOY");
-insert into item_hats (name, price, stock, description, itemImageUrl, genre) values (4, "Sherlock Holmes Hat", 39.99, "Limited Edition!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_3.jpg", "BEANIE");
-insert into item_hats (name, price, stock, description, itemImageUrl, genre) values (5, "Twitches 2.0", 15.99, "Happy Halloween!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_4.jpg", "BEANIE");
+insert into item_hats (name, price, stock, description, itemImageUrl, genre) values ( "Sunhat 3000", 29.99, 20, "Keep the sun out of your face!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_6.jpg", "SUN");
+insert into item_hats (name, price, stock, description, itemImageUrl, genre) values ( "Baseball M Hat", 59.99, 10, "Super cool!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_1.jpg", "BASEBALL");
+insert into item_hats (name, price, stock, description, itemImageUrl, genre) values ( "Rodeo Hat", 23.99, "Square dancin'!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_2.jpg", "COWBOY");
+insert into item_hats (name, price, stock, description, itemImageUrl, genre) values ( "Sherlock Holmes Hat", 39.99, "Limited Edition!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_3.jpg", "BEANIE");
+insert into item_hats (name, price, stock, description, itemImageUrl, genre) values ( "Twitches 2.0", 15.99, "Happy Halloween!", "https://200727-java-ng-usf-gu.s3.us-east-2.amazonaws.com/assets/img/hat_4.jpg", "BEANIE");
 
